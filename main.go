7de0de8601169fdec149ce00e3f022e995707610
Swexbe <erik@erikjohnsson.se>
2019-10-16 15:03:33 +0200

    Read file

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+
+	"golang.org/x/text/encoding/charmap"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	b, err := ioutil.ReadFile("rep.xls")
+
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	str := string(b)
+
+	f, err := charmap.ISO8859_1.NewDecoder().String(str)
+
+
 }
